82d08b866525843d003c0b20beb4b8251ff9c650
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2024-05-19 18:09:04 +0200

    label and remove lib_ml dep

--- backend/app.py
+++ backend/app.py
@@ -13,9 +13,7 @@ def predict():
 
 @app.route('/version', methods=['GET'])
 def version():
-    # Assuming lib-version is a Python package that has a __version__ attribute
-    import lib_ml
-    return jsonify({"version": lib_ml.__version__})
+    return jsonify({"version": 'dummy'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
