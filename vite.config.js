5c5eeda1073a6dd8508c122d22dc3983e2ee4c63
Dielof van Loon <d.i.vanloon@student.tudelft.nl>
2024-05-21 20:50:33 +0200

    fixed ip bug

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,  // Ensure this port is not in use
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://app-backend:5000',
         changeOrigin: true,
         secure: false,
       }
